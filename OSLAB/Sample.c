45f882cd1c90da624a202139202d6e36481cb627
Jasmin G <94514747+204G1A0542@users.noreply.github.com>
2022-08-30 12:29:34 +0530

    Jasmin GCompleted Sample.c with C source code

--- OSLAB/Sample.c
+++ OSLAB/Sample.c
@@ -5,7 +5,15 @@ Date of Creation: 30-08-2022
 Date of Execution: 30-08-2022
 */
 // Source Code
-#include <stdio.h>
-int main(){
-printf("Hello World\n");
-}
+#include<stdio.h>  
+int main()    
+{    
+ int i,fact=1,number;    
+ printf("Enter a number: ");    
+  scanf("%d",&number);    
+    for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  printf("Factorial of %d is: %d",number,fact);    
+return 0;  
+}   
